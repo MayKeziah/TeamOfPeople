494267cee7549412ea876e1f5b9f6ea183d722f5
MayKeziah <keziah.may@gmail.com>
2021-05-24 20:27:30 -0700

    added map creation to main

---
+++ DogChain/Assets/Alpha files/Scripts/GenerateWalls/SerializeList.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class SerializeList
+{
+
+  public void serialize(List<List<GameObject>> list, string destination)
+  {
+    using(StreamWriter writetext = new StreamWriter(destination))
+    {
+        writetext.WriteLine("public Vector2[][] Perm1 = {");
+        int wallCount = 0;
+        foreach(List<GameObject> wall in list)
+        {
+          writetext.WriteLine("new Vector2[]{");
+          int counter = 0;
+          foreach(GameObject brick in wall)
+          {
+            writetext.WriteLine("new Vector2(" 
+             + brick.transform.position.x + "f, "
+             + brick.transform.position.y + "f)");
+             if (counter != wall.Count - 1) writetext.WriteLine(", ");
+             counter++;
+          }
+          writetext.WriteLine("}");
+          if (wallCount != list.Count - 1) writetext.WriteLine(", ");
+          wallCount++;
+        }
+        writetext.WriteLine("};");
+    }
+  }
+}
